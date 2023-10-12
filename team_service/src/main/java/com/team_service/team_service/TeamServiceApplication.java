7216c7cf6d6b718f572ee8424185886886afac5e
jeanForconi <forconijean@gmail.com>
2023-10-12 13:55:56 +0200

    mise en place du serveur eureka

--- team_service/src/main/java/com/team_service/team_service/TeamServiceApplication.java
+++ team_service/src/main/java/com/team_service/team_service/TeamServiceApplication.java
@@ -2,8 +2,10 @@ package com.team_service.team_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TeamServiceApplication {
 
 	public static void main(String[] args) {
